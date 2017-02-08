f89351572a9353ca97ec9e8c5ce5af64cfb84d9c
brunoterkaly <brunoterkaly@hotmail.com>
2017-02-08 18:58:53 +0000

    first commit

--- do.sh
+++ do.sh
@@ -1,5 +1,6 @@
 git init
 git add --all
+git rm kubernetes/list.txt
 git commit -m "first commit"
 git remote add origin https://github.com/brunoterkaly/BigLittleChallenge.git
 git push -u origin master
